90e27d42aa16fe94a054c7f97480a20eb389a8dd
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2024-09-12 16:51:42 -0400

    Added copy element and first and last names to students enrolled page

--- ai_app/views/dashboard_views.py
+++ ai_app/views/dashboard_views.py
@@ -93,11 +93,16 @@ def students_enrolled(request, room_code):
 def student_questions(request, room_code):
     classroom = get_object_or_404(ClassRoom, room_code=room_code)
     questions = Question.objects.filter(user__schooluserprofile__classes=classroom)
-    return render(request, 'ai_app/dashboards/student_questions.html', {'classroom': classroom, 'questions': questions})
+    return render(request, 'ai_app/dashboards/teacher/student_questions.html', {'classroom': classroom, 'questions': questions})
 
 
 @login_required
 def students_in_class(request, room_code):
+    """
+    Looks through ClassRoom table to see if room_code exists. 
+    Filters through SchoolUserProfile to find all students in the class.
+    Renders the HTML page students_in_class.html and passes classroom and students data to the template.
+    """
     classroom = get_object_or_404(ClassRoom, room_code=room_code)
     students = SchoolUserProfile.objects.filter(classes=classroom, role='student')
     return render(request, 'ai_app/dashboards/students/students_in_class.html', {'classroom': classroom, 'students': students})
