62c44244472cb53160cd964b981ceac2307192f8
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2024-10-28 12:47:16 -0400

    Added more data to submission list page

--- ai_app/views/teacher_dashboard_views.py
+++ ai_app/views/teacher_dashboard_views.py
@@ -367,7 +367,7 @@ class SubmissionsListView(ListView):
 
     def get_queryset(self):
         assignment_id = self.kwargs['assignment_id']  # Get assignment_id from URL
-        return Submissions.objects.filter(assignment_id=assignment_id)
+        return Submissions.objects.filter(assignment_id=assignment_id).select_related('student_profile__user')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
